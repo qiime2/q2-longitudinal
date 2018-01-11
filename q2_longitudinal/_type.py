3f9b864ca188e77c78cce4bf52dec10e023fedc4
Evan Bolyen <ebolyen@gmail.com>
2018-01-11 11:45:55 -0700

    MAINT: update copyright year; travis

--- q2_longitudinal/_type.py
+++ q2_longitudinal/_type.py
@@ -1,5 +1,5 @@
 # ----------------------------------------------------------------------------
-# Copyright (c) 2017, QIIME 2 development team.
+# Copyright (c) 2017-2018, QIIME 2 development team.
 #
 # Distributed under the terms of the Modified BSD License.
 #
