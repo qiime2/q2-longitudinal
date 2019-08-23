6e816ba1455ac66d810c33b668602aad13b19402
Nicholas Bokulich <nbokulich@gmail.com>
2019-08-23 08:11:57 -0700

    IMP: added cividis colormap (#145)

--- q2_longitudinal/plugin_setup.py
+++ q2_longitudinal/plugin_setup.py
@@ -73,7 +73,7 @@ base_parameters = {
     'palette': Str % Choices([
         'Set1', 'Set2', 'Set3', 'Pastel1', 'Pastel2', 'Paired', 'Accent',
         'Dark2', 'tab10', 'tab20', 'tab20b', 'tab20c', 'viridis', 'plasma',
-        'inferno', 'magma', 'terrain', 'rainbow']),
+        'inferno', 'magma', 'terrain', 'rainbow', 'cividis']),
 }
 
 paired_params = {
