daa093c981d14932b4d5ab10c4f227a5dcc60135
q2d2 <q2d2.noreply@gmail.com>
2022-01-03 14:45:29 +0000

    MAINT: Updating copyright year

--- q2_longitudinal/_format.py
+++ q2_longitudinal/_format.py
@@ -1,5 +1,5 @@
 # ----------------------------------------------------------------------------
-# Copyright (c) 2017-2021, QIIME 2 development team.
+# Copyright (c) 2017-2022, QIIME 2 development team.
 #
 # Distributed under the terms of the Modified BSD License.
 #
