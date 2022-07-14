0acc64968ca57387e772097142dfaae8abe8d7a7
sterrettJD <sterrettjd@protonmail.com>
2022-07-14 08:33:32 -0600

    update RM anova argument registration

--- q2_longitudinal/plugin_setup.py
+++ q2_longitudinal/plugin_setup.py
@@ -271,14 +271,21 @@ plugin.visualizers.register_function(
                    'columns. ' + formula_description,
         'sstype': (
             'Type of sum of squares calculation to perform (I, II, or III).'),
-        'repeated_measures': 'Perform ANOVA as a repeated measures ANOVA',
+        'repeated_measures': 'Perform ANOVA as a repeated measures ANOVA. '
+                             'Implemented via statsmodels, which has the '
+                             'following limitations: Currently, only '
+                             'fully balanced within-subject designs are '
+                             'supported. Calculation of between-subject '
+                             'effects and corrections for violation of '
+                             'sphericity are not yet implemented.',
         'individual_id_column': 'The column containing individual ID with '
                                 'repeated measures to account for.'
                                 'This should not be included in the formula.',
         'rm_aggregate': 'If the data set contains more than a single '
                         'observation per individual id and cell of the specified '
                         'model, this function will be used to aggregate '
-                        'the data by the mean before running the ANOVA.'
+                        'the data by the mean before running the ANOVA. '
+                        'Only applicable for repeated measures ANOVA. '
     },
     name='ANOVA test',
     description=('Perform an ANOVA test on any factors present in a metadata '
