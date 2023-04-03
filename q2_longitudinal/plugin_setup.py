2c592bc8999410f1b802951487d368efe3e85905
Liz Gehret <elizabeth.gehret@gmail.com>
2023-04-03 15:31:51 -0700

    squash

--- q2_longitudinal/plugin_setup.py
+++ q2_longitudinal/plugin_setup.py
@@ -30,7 +30,6 @@ from ._longitudinal import (pairwise_differences, pairwise_distances,
                             first_differences, first_distances,
                             maturity_index, feature_volatility,
                             plot_feature_volatility, anova)
-
 import q2_longitudinal
 
 citations = Citations.load('citations.bib', package='q2_longitudinal')
@@ -319,7 +318,7 @@ plugin.visualizers.register_function(
         'over time is co-plotted as "spaghetti" plots if the '
         '"individual_id_column" parameter is used. state_column will '
         'typically be a measure of time, but any numeric metadata column can '
-        'be used.'),
+        'be used.')
 )
 
 
